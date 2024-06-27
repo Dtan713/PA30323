b85f6e14384b76b97f52b23ba79d12d4e8592681
Dtan713 <s.donaldtan@gmail.com>
2024-06-27 18:58:12 -0400

    first commit

---
+++ src/PA30323part1.java
@@ -0,0 +1,21 @@
+public class BitShift  {
+    public static void main(String[] args) {
+        // Intial value 2
+        int x = 2;
+        System.out.println("Original value of x: " + x);
+        System.out.println("Binary string of x: " + int.toBinaryString(x));
+
+        x = y << 1;
+        System.out.println("After left shift by 1, x: " + x);
+        System.out.println("Binary string of x: " +);
+
+        x = 9;
+        System.out.println("\nOriginal value of x: " + x);
+
+        x = x << 1;
+        System.out.println();
+
+
+
+    }
+}
