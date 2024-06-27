07e6f4d297db18ff584247b0d6128bc3914e619c
Dtan713 <s.donaldtan@gmail.com>
2024-06-27 19:04:50 -0400

    adding more

--- src/BitShift.java
+++ src/BitShift.java
@@ -3,16 +3,19 @@ public class BitShift  {
         // Intial value 2
         int x = 2;
         System.out.println("Original value of x: " + x);
-        System.out.println("Binary string of x: " + int.toBinaryString(x));
+        System.out.println("Binary string of x: " + integer.toBinaryString(x));
 
         x = y << 1;
         System.out.println("After left shift by 1, x: " + x);
-        System.out.println("Binary string of x: " +);
+        System.out.println("Binary string of x: " + Intetger.toBinaryString(x));
 
         x = 9;
         System.out.println("\nOriginal value of x: " + x);
 
         x = x << 1;
+        System.out.println("After left shift by 1, x: " + x);
+
+        x=17;
         System.out.println();
 
 
